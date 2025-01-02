06472e08d0cbc4ff2b1d67be6b8460e0b094f677
seonjuuu <mhnaa6469@gmail.com>
2025-01-02 14:31:53 +0900

    self_intro

---
+++ script.js
@@ -0,0 +1,108 @@
+/*다크 모드*/
+const profilePhoto = document.querySelector(".profile_photo");
+
+profilePhoto.addEventListener("click", () => {
+    //if (document.body.className == "dark-mode") {
+    //    document.body.className = '';
+    //} else {
+    //    document.body.className = 'dark-mode';
+    //}
+
+    // DOM 요소에 지정한 클래스 값이 없으면 추가하고, 있다면 제거
+    document.body.classList.toggle('dark-mode');
+})
+
+/*스크롤 모드*/
+/* forEach 함수 이용 */
+/* 함수화 */
+
+
+fetch("https://m.search.naver.com/p/csearch/content/apirender.nhn?where=nexearch&pkid=387&u2=20020910&q=생년월일+운세&u1=f&u3=solar&u4=12&_=1719518803829")
+    .then((response) => response.json())  // 응답을 JSON으로 파싱
+    .then(data => {
+        const htmlString = data.flick[0]; // 첫 번째 항목 선택
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(htmlString, 'text/html');
+        const fortune = doc.querySelector('dd b').textContent;
+        const fortuneText = doc.querySelector('dd p').textContent;
+        console.log(fortune); // 추출한 텍스트 출력
+        console.log(fortuneText); // 추출한 텍스트 출력
+
+        const fortuneSection = document.createElement("section");
+        const sectionTitle = document.createElement("h2");
+        sectionTitle.textContent = "오늘의 운세"
+        const fortuneE = document.createElement("h3")
+        fortuneE.style.margin = 0;
+        fortuneE.textContent = fortune;
+        const fortuneTextE = document.createElement("p");
+        fortuneText.textContent = fortuneText;
+        
+        fortuneSection.append(sectionTitle);
+        fortuneSection.append(fortuneE);
+        fortuneSection.append(fortuneTextE);
+
+        const contactSection = document.querySelector(".contact");
+        contactSection.after(fortuneSection);
+
+        initializeSection();
+
+    });
+
+
+    function initializeSection() {
+        const sections = document.querySelectorAll(".right_container section");
+        let currentIndex = 0;
+
+        const showAfterSection = () => {
+            sections.forEach((section) => { section.style.display = 'none'; })  // 현재 section 숨기기
+            if (currentIndex == sections.length - 1) {
+                currentIndex = 0;
+            } else {
+                currentIndex++;
+            }
+            sections[currentIndex].style.display = 'flex';  // 다음 section 보여주기
+        }
+        const showBeforeSection = () => {
+            sections.forEach((section) => { section.style.display = 'none'; })
+            if (currentIndex == 0) {
+                currentIndex = sections.length - 1;
+            } else {
+                currentIndex--;
+            }
+            sections[currentIndex].style.display = 'flex';
+        }
+
+        /*함수화*/
+        let intervalId = setInterval(showAfterSection, 3000);
+
+        const resetInterval = () => {
+            clearInterval(intervalId);
+            intervalId = setInterval(showAfterSection, 3000);
+        }
+
+        sections.forEach((section, index) => {
+            section.addEventListener("click", (event) => {
+                const sectionWidth = section.offsetWidth;
+                const clickX = event.clientX - section.getBoundingClientRect().left;
+
+                if (clickX < sectionWidth / 3) {  // 왼쪽 1/3 클릭 시 이전 section으로 이동
+                    showBeforeSection();
+                    resetInterval();
+                } else if (clickX > sectionWidth * 2 / 3) {  // 오른쪽 1/3 클릭 시 다음 section으로 이동
+                    showAfterSection();
+                    resetInterval();
+                } else {  // 중간 1/3 클릭 시 자동 넘김 토글
+                    if (intervalId) {
+                        clearInterval(intervalId);  // 자동 넘김 중지
+                        intervalId = null;
+                    } else {
+                        intervalId = setInterval(showAfterSection, 3000);  // 자동 넘김 재개
+                    }
+                }
+            });
+        });
+    }
+
+
+
+            
